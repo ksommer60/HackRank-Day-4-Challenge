57f6c9df8526fed625454466e06daf2d7b2ce319
ksommer60 <105603761+ksommer60@users.noreply.github.com>
2022-09-21 06:57:46 -0400

    Add files via upload

---
+++ HackerChallengeDay4/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerChallengeDay4
+{
+    class Person
+    {
+        public int age;
+
+        public Person(int initialAge)
+        {
+            if (initialAge <  0)
+            {
+                age = 0;
+                Console.WriteLine("Age is not valid, setting age to 0.");
+            }
+            else
+            {
+                age = initialAge; 
+            }
+        }
+
+        public void AmIOld()
+        {
+            if (age < 13)
+            {
+                Console.WriteLine("You are young.");
+            }
+            else if (age >= 13 && age < 18)
+            {
+                Console.WriteLine("You are a teenager."); 
+            }
+            else
+            {
+                Console.WriteLine("You are old.");
+            }
+        }
+
+        public void YearPasses()
+        {
+            age += 1;
+        }
+    }
+}
---
+++ HackerChallengeDay4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HackerChallengeDay4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.In.ReadLine());
+            
+            for (int i = 0; i < T; i++)
+            {
+                int age = int.Parse(Console.In.ReadLine());
+                Person p = new Person(age);
+                p.AmIOld();
+                
+                for (int j = 0; j < 3; j++)
+                {
+                    p.YearPasses();
+                }
+                p.AmIOld();
+                Console.WriteLine();
+            }
+        }
+    }
+}
